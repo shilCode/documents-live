de3d72bd88e549921f8b63da28ace426ea2dab19
shilcode <2850542@gmail.com>
2024-07-27 12:56:09 +0200

    auth done

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,13 @@
+import { SignIn } from '@clerk/nextjs'
+import React from 'react'
+
+const SigninPage = () => {
+  return (
+    <main className='auth-page'>
+        <SignIn/>
+
+    </main>
+  )
+}
+
+export default SigninPage
